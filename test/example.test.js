35bf2f71cedaccb93131ebb27a4d16ebaa76d670
Frank Hollywood <github@srvsync.com>
2021-08-24 18:27:29 -0700

    added testing

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,31 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
+import { findById } from '../findById.js';
+import { candies } from '../data.js';
+
+
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('Should take in an item from the cart and render a <tr>. ', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = { 
+        id: 2, 
+        name: 'Peanut Butter Cups',
+        img: './assets/Reeses.jpg',
+        price: 2,
+        quantity: 2,
+        weight: .5
+    };
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = findById( candies, 2);
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.deepEqual(actual, expected);
 });
+
+
